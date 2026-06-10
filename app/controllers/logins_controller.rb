40bed7df03927265b4131b002e9628f1b8fba042
yuunagi <yuunagi0421@yahoo.co.jp>
2026-06-10 19:45:00 +0900

    add:ログインしていない場合はログイン画面に遷移

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,4 +1,5 @@
 class LoginsController < ApplicationController
+    skip_before_action :require_login, only: %i[new create]
     def new
     end
 
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,5 @@
 class TopController < ApplicationController
+    skip_before_action :require_login, only: %i[index]
     def index
         @today = Date.today
         @day_of_week = %w[日 月 火 水 木 金 土][@today.wday]
