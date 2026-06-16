57a907e79ee474a9b0dba2958cb607443c7e422f
yuunagi <yuunagi0421@yahoo.co.jp>
2026-06-16 20:17:19 +0900

    add:メインページのUI追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.assets.debug = false
   # Settings specified here will take precedence over those in config/application.rb.
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
