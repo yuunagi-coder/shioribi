c69b44c58bd0e8894e0940796be4ff163d515ab4
yuunagi <yuunagi0421@yahoo.co.jp>
2026-07-22 20:56:50 +0900

    fix:current_userがnilになる問題を解決

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,9 +1,10 @@
 class TopController < ApplicationController
     skip_before_action :require_login, only: %i[index]
+    
     def index
         @today = Date.today
         @day_of_week = %w[日 月 火 水 木 金 土][@today.wday]
-        @post = current_user.posts.sample 
+        @post = current_user&.posts.sample 
     end
 
     def introduction
