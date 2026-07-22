40bc03a3f7d851560ee4f19728fa6407b3500836
yuunagi <yuunagi0421@yahoo.co.jp>
2026-07-22 20:25:02 +0900

    add:道しるべにランダムな一文を表示

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,6 +3,7 @@ class TopController < ApplicationController
     def index
         @today = Date.today
         @day_of_week = %w[日 月 火 水 木 金 土][@today.wday]
+        @post = current_user.posts.sample 
     end
 
     def introduction
